227e30d751fc91f5903329955aedc089cfca2144
kis9a <kis9ax@gmail.com>
2021-01-14 00:44:14 +0900

    feat: add StateCounter.jsx

---
+++ react/src/components/StateCounter.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+// initialize random number 1~10
+const initialState = Math.floor(Math.random() * 10) + 1;
+
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: initialState,
+      open: true,
+    };
+  }
+  toggle = () => {
+    this.setState({ open: !this.state.open });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="m1">
+          <button onClick={this.toggle}>
+            {this.state.open ? "Close" : "Open"}
+          </button>
+          <div className={this.state.open ? "isOpen" : "isClose"}>
+            <p className="p1">Current number is {this.state.count}</p>
+            <button
+              onClick={() => this.setState({ count: this.state.count + 1 })}
+            >
+              {" "}
+              + 1{" "}
+            </button>
+            <button
+              onClick={() => this.setState({ count: this.state.count - 1 })}
+            >
+              {" "}
+              - 1{" "}
+            </button>
+            <button onClick={() => this.setState({ count: 0 })}>0</button>
+            <button onClick={() => this.setState({ count: initialState })}>
+              Initialize
+            </button>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
+
+export default Counter;
